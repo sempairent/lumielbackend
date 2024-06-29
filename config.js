df88718d051634e91f3524a03c3d029d114cce48
valentine <sempairent@gmail.com>
2024-06-28 23:14:40 -0500

    conf_brench

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3001
-export const MYSQLUSER  = process.env.MYSQLUSER || 'root'
-export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
-export const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
+export const MYSQLUSER  = process.env.MYSQLUSER || 'urvlyyxpvsanh5w6'
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'QcN16dhJMuC0gF0JhMDF'
+export const MYSQLHOST = process.env.MYSQLHOST || 'b8zh0cpbkslszwu4tqc7-mysql.services.clever-cloud.com'
 
-export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'reproductor_musica'
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'b8zh0cpbkslszwu4tqc7'
 export const MYSQLPORT = process.env.MYSQLPORT || 3306
